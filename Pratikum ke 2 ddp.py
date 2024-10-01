6d93416eccf471948eb3d7a01cc5770be841d685
MuhammadRafly12 <muhammadrafly.pp@gmail.com>
2024-09-30 21:04:17 -0700

    Add files via upload

---
+++ Pratikum ke 2 ddp.py
@@ -0,0 +1,10 @@
+
+umur = int(input("Masukkan umur"))
+
+if umur <=5:
+    print ("umur anda balita")
+elif umur >=12:
+    print ("anda SMP")
+elif umur >=18:
+    print("anda sigma skibidi gyatt")
+
